82b8dc4b649a9916416c53dd735dc13e87447b67
Shubham Daule <sbdaule5@gmail.com>
2024-04-09 18:07:57 +0530

    StudentPortal: Remove the sidebar from login page

--- StudentPortal/src/lib/components/Sidebar.svelte
+++ StudentPortal/src/lib/components/Sidebar.svelte
@@ -3,7 +3,7 @@
 <script>
     /** @type Array.<{name: string; url:string;}> */
     let links = [
-        { name: "Announcements", url: "/" },
+        { name: "Announcements", url: "/annoucements" },
         { name: "New Complain", url: "/complain/new" },
         { name: "View Complains", url: "/complain" },
         { name: "Logout", url: "/logout" },
