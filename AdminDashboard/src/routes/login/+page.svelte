924c17fc647a229155ea5460dd16ff982457f1bc
Shubham Daule <sbdaule5@gmail.com>
2024-04-10 07:29:39 +0530

    AdminDashboard: Fix login after migration

--- AdminDashboard/src/routes/login/+page.svelte
+++ AdminDashboard/src/routes/login/+page.svelte
@@ -1,51 +1,8 @@
-<!-- <script>
-    import Cookies from "js-cookie";
-    import { DefaultApi } from "$lib/generated";
-    import { onDestroy } from "svelte";
-
-    let username = "";
-    let password = "";
-
-    const api = new DefaultApi();
-
-    async function handleLogin() {
-        try {
-            let res = await api.login({
-                loginRequest: {
-                    username: username,
-                    password: password,
-                },
-            });
-            setCookie("authToken", res);
-            window.location.href = "/";
-        } catch (e) {
-            console.log("Invalid username or password");
-        }
-    }
-
-    /**
-     * Set a cookie with the provided key and value.
-     * @param {string} key - The key for the cookie.
-     * @param {string} value - The value to set for the cookie.
-     */
-    function setCookie(key, value) {
-        Cookies.set(key, value, { expires: 1 });
-    }
-
-</script>
-
-<h1>LOGIN PAGE</h1>
-
-<form on:submit|preventDefault={handleLogin}>
-    <input type="text" placeholder="Username" bind:value={username} />
-    <input type="text" placeholder="Password" bind:value={password} />
-    <button type="submit">Login</button>
-</form> -->
-
 <script>
     import Cookies from "js-cookie";
-    import { DefaultApi } from "$lib/generated";
     import { goto } from "$app/navigation";
+    import { getDefaultApi } from "$lib/utils/auth";
+    import { DefaultApi, Role } from "$lib/generated";
 
     let username = "";
     let password = "";
@@ -58,6 +15,7 @@
                 loginRequest: {
                     username: username,
                     password: password,
+                    role: Role.Admin,
                 },
             });
             setCookie("authToken", res);
